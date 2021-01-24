4422f7eba3a89f2628ea8454af9d00ba0746d64b
Cavin Gayle <cavingayle@gmail.com>
2021-01-23 21:56:53 -0500

    removed errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./components/navbar/Navbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 
